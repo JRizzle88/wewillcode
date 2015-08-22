b6251ff83f83e665a13e5e22f7f3115850411b4a
JRizzle88 <jrizzle88@yahoo.com>
2015-08-22 10:38:28 -0500

    quick page fix

--- resources/views/superadmin/pages/index.blade.php
+++ resources/views/superadmin/pages/index.blade.php
@@ -16,7 +16,7 @@
 								<div>
 								
 								@if($p->draft == 1)
-									<a href="{{ route('admin.pages.edit', $p->slug) }}">
+									<a href="{{ route('superadmin.pages.edit', $p->slug) }}">
 										<span class="text-warning"><i class="fa fa-exclamation-triangle"></i> Draft</span> - {{ $p->name }}
 									</a>
 								@else
