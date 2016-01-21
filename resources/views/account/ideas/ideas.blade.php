a296bfafc18bb77b4005ed9a7ca21e7cf8610a41
JRizzle88 <jrizzle88@yahoo.com>
2016-01-21 08:33:37 -0600

    added idea lets get started outside of ideas loop

--- resources/views/account/ideas/ideas.blade.php
+++ resources/views/account/ideas/ideas.blade.php
@@ -9,27 +9,30 @@
             <p>Submit your first idea!</p>
             <p><a href="{{ url('account/ideas/process')}}" class="btn wwc-green-bg"> Let's Get Started</a></p>
         @else
-            <div class="single-idea">
+            <div class="ideas">
                 @foreach($ideas as $i)
-                    <div class="single-idea-listing">
-                        <span class="idea-id">{{ $i->id }}</span>
-                        <span class="idea-name">{{ $i->name }}</span>
-                        <span class="idea-actions">
-                            <a href=""><i class="fa fa-eye"></i></a>
-                            <a href=""><i class="fa fa-edit"></i></a>
-                            <a href=""><i class="fa fa-envelope"></i></a>
-                        </span>
-                    </div>
-                    <div class="single-idea-listing-data" style="display:none">
-                        <p>{{ $i->description_short }}</p>
-                        <p>{{ $i->description_long_1 }}</p>
-                        <p>{{ $i->description_long_2 }}</p>
-                        <p>{{ $i->ideaDetails->funding_goal }}</p>
-                        <p>{{ $i->ideaDetails->date_start }}</p>
-                        <p>{{ $i->ideaDetails->date_end }}</p>
+                    <div class="single-idea">
+                        <div class="single-idea-listing">
+                            <span class="idea-id">{{ $i->id }}</span>
+                            <span class="idea-name">{{ $i->name }}</span>
+                            <span class="idea-actions">
+                                <a href=""><i class="fa fa-eye"></i></a>
+                                <a href=""><i class="fa fa-edit"></i></a>
+                                <a href=""><i class="fa fa-envelope"></i></a>
+                            </span>
+                        </div>
+                        <div class="single-idea-listing-data" style="display:none">
+                            <p>{{ $i->description_short }}</p>
+                            <p>{{ $i->description_long_1 }}</p>
+                            <p>{{ $i->description_long_2 }}</p>
+                            <p>{{ $i->ideaDetails->funding_goal }}</p>
+                            <p>{{ $i->ideaDetails->date_start }}</p>
+                            <p>{{ $i->ideaDetails->date_end }}</p>
+                        </div>
                     </div>
                 @endforeach
             </div>
+            <p><a href="{{ url('account/ideas/process')}}" class="btn wwc-green-bg"> Let's Get Started</a></p>
         @endif
     </div>
 </div>
